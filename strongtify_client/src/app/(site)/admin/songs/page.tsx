d3d69a10386f247faeabc35186ccf7259e055213
VuManh1 <vubamanh05@gmail.com>
2023-10-10 22:51:45 +0700

    remove metadata in admin songs page

--- strongtify_client/src/app/(site)/admin/songs/page.tsx
+++ strongtify_client/src/app/(site)/admin/songs/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import type { Metadata } from "next";
 import Image from "next/image";
 
 import TableItem from "@/components/admin/tables/TableItem";
 import { getSongs, searchSongs } from "@/services/api/songs";
 import { NO_IMAGE_URL } from "@/libs/constants";
-
-export const metadata: Metadata = {
-    title: "Quản lý Songs",
-};
 
 const songColumns = [
     {
