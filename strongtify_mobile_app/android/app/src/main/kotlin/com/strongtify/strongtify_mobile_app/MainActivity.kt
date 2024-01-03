632d8e7236bd496c85e3b8c637fe49b169adb335
Mạnh Vũ <vubamanh05@gmail.com>
2024-01-03 12:18:34 +0700

    Add flutter project

---
+++ strongtify_mobile_app/android/app/src/main/kotlin/com/strongtify/strongtify_mobile_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.strongtify.strongtify_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ strongtify_mobile_app/lib/components/button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  const Button(
+      {super.key,
+      required this.buttonText,
+      this.isOutlined = false,
+      required this.onPressed,
+      this.width = 280});
+
+  final String buttonText;
+  final bool isOutlined;
+  final Function onPressed;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onPressed();
+      },
+      child: Material(
+        borderRadius: BorderRadius.circular(30),
+        elevation: 4,
+        child: Container(
+          width: width,
+          padding: const EdgeInsets.all(13),
+          decoration: BoxDecoration(
+            color: isOutlined ? Colors.white : Colors.orange[400],
+            border: Border.all(color: Colors.orange[400]!, width: 2.5),
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: isOutlined ? Colors.orange[400] : Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
