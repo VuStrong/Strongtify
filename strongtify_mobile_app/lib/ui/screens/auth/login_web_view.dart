5cb56b0f78dd02dfa0c413dc626282174407a6b0
Mạnh Vũ <vubamanh05@gmail.com>
2024-02-21 21:21:31 +0700

    Fix push screen logic in register_screen

--- strongtify_mobile_app/lib/ui/screens/auth/login_web_view.dart
+++ strongtify_mobile_app/lib/ui/screens/auth/login_web_view.dart
@@ -27,9 +27,9 @@ class _LoginWebViewState extends State<LoginWebView> {
       ..setBackgroundColor(const Color(0x00000000))
       ..setUserAgent(
         Platform.isIOS
-            ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15' +
+            ? 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_2 like Mac OS X) AppleWebKit/605.1.15'
                 ' (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1'
-            : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) ' +
+            : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
                 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36',
       )
       ..addJavaScriptChannel(
--- strongtify_mobile_app/lib/ui/screens/auth/register_screen.dart
+++ strongtify_mobile_app/lib/ui/screens/auth/register_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:strongtify_mobile_app/common_blocs/auth/bloc.dart';
 import 'package:strongtify_mobile_app/ui/screens/auth/login_web_view.dart';
+import 'package:strongtify_mobile_app/ui/widgets/bottom_navigation_app.dart';
 import 'package:strongtify_mobile_app/ui/widgets/button.dart';
 import 'package:strongtify_mobile_app/ui/widgets/text_input.dart';
 import 'package:strongtify_mobile_app/ui/screens/auth/confirm_email_screen.dart';
@@ -72,8 +73,13 @@ class _RegisterScreenState extends State<RegisterScreen> {
           }
 
           if (state.user != null && context.mounted) {
-            Navigator.pushNamedAndRemoveUntil(
-                context, ConfirmEmailScreen.id, (route) => false);
+            if (state.user!.emailConfirmed) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, BottomNavigationApp.id, (route) => false);
+            } else {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, ConfirmEmailScreen.id, (route) => false);
+            }
           }
         },
         child: Scaffold(
